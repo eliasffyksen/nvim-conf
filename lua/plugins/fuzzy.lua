9843c2b9bf09889add3a7e2461fba302d407a4ee
Elias F. Fyksen <github@fyksen.net>
2024-03-06 16:42:49 +0000

    Ignore dot directories in fzf

--- lua/plugins/fuzzy.lua
+++ lua/plugins/fuzzy.lua
@@ -1,4 +1,9 @@
 return {
 	'ibhagwan/fzf-lua',
 	dependencies = {'nvim-tree/nvim-web-devicons'},
+	opts = {
+		files = {
+			find_opts = [[-type f -not -path '*/\.*/*' -printf '%P\n']]
+		},
+	},
 }
